e9e318a74c625fc4647ce08177fe978832f3e25d
ayurn <65171090+ayurn@users.noreply.github.com>
2020-06-08 23:35:13 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for
+import requests
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+	data = requests.get("https://disease.sh/v2/countries/India?yesterday=true&strict=true")
+	data_dict = data.json()
+	return render_template("home.html", data = data_dict)
+
+ 
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
